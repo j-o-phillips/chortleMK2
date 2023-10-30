088dd9c6f7847bfc39110c792241ecdab3dc246c
rowkee <rick.keenan10@gmail.com>
2023-10-30 12:54:39 +0000

    added the most basic navbar

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar/Navbar"
 
 export default function Home() {
-  return <div>App</div>;
+ 
+  return (
+    <>
+      <Navbar />
+      <div>App</div>
+    </>
+  )
 }
