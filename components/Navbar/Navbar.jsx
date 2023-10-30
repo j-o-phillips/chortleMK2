d38249a77e30f5d99b590a891d07c64455d727e4
rowkee <rick.keenan10@gmail.com>
2023-10-30 20:10:37 +0000

    adding img and profile info to navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import LogOutBtn from "../LogOutBtn/LogOutBtn";
 import style from "./Navbar.module.css"
+import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 function Navbar() {
+const session = useSession()
   return (
       <nav className={style.nav}>
-        <div>
-          <div>Image</div>
-          <div > Household Name</div>
-        </div>
-        <div>
+        <div className="navbar-right">
+          <Image width={20} height={20} src="" />
+          <div > Household Name </div>
+        </div >
+        <div className="navbar-right">
+          <p>{session.data.user.name}</p>
+          <Image width={20} height={20} src={session.data.user.image} />
           <LogOutBtn />
         </div>
       </nav>
