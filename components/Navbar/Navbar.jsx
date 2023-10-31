e67a0c90bd71645747d63d46f75b96fba1146101
rowkee <rick.keenan10@gmail.com>
2023-10-31 16:38:25 +0000

    added some styling to nav, body and sidebar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@
 import { useContext, useEffect } from "react";
 import LogOutBtn from "../LogOutBtn/LogOutBtn";
 import style from "./Navbar.module.css";
-// import Image from "next/image";
+import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { UserContext } from "@/context/UserContext";
 
@@ -12,18 +12,15 @@ function Navbar() {
   const { user } = useContext(UserContext);
 
   useEffect(() => {
-    console.log(user);
   }, [user]);
 
   return (
     <nav className={style.nav}>
-      <div className={style.logo}>Image</div>
+      <div className={style.logo}>
+      </div>
       <div className={style.end}>
         <div>{user}</div>
-        <button className={style.button}>
-          LogOut
           <LogOutBtn />
-        </button>
       </div>
     </nav>
   );
