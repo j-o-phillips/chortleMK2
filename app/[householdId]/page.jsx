7cdfdc4ed0de8338b0a49a0edaed5a652a1d5aaf
ivanjorge <ivandcarvalho29@gmail.com>
2023-10-31 10:31:09 +0000

    sidebar resized

--- app/[householdId]/page.jsx
+++ app/[householdId]/page.jsx
@@ -1,21 +1,13 @@
 "use client";
 
-import { Button, Stack } from "react-bootstrap"
-import Container from "react-bootstrap/Container"
+
 import ChoreCard from "@/components/ChoreCard/ChoreCard";
+import { Card } from "react-bootstrap";
 
 function Dashboard() {
   return (
-    <>
-    <Stack direction="horizontal" gap="2" className="mb-4 mt-4" >
-      <h1 className="me-auto">Dashboard</h1>
-      <Button variant="primary">Create Chore</Button>
-      {/* <Button variant="outline-primary" onClick='check done'>Check done</Button> */}
-    </Stack>
-    <Container>
-          < ChoreCard />
-  </Container>
-  </>
+    <ChoreCard/>
+   
   );
 }
 
