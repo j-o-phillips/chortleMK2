e7025e45bb39b2fc172e12673096ca6e1e063a36
rowkee <rick.keenan10@gmail.com>
2023-10-31 12:28:48 +0000

    profile photo attempt

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,20 +2,30 @@
 
 import LogOutBtn from "../LogOutBtn/LogOutBtn";
 import style from "./Navbar.module.css"
-// import Image from "next/image";
-import { useSession } from "next-auth/react";
+import { useContext, useState } from "react";
+import { UserContext } from "@/context/UserContext";
+import Image from "next/image";
+
 
 function Navbar() {
-// const session = useSession()
-  return (
+  const user = useContext(UserContext)
+
+  if (!user) {
+    return null
+  }
+  
+  console.log(user)
+  
+    return (
       <nav className={style.nav}> 
           <div className={style.logo}>Image</div> 
+          {/* <Image src={user.image} height={10} width={10} alt="profile pic"></Image> */}
+          <p>{user.name}</p>
+          <div></div>
           <div className={style.end}>
-          <div> Household Name</div>
-        <button className={style.button}>
-          LogOut
-          <LogOutBtn />
-        </button>
+            <div> Household Name</div>
+            {/* logout button only appear if you are logged in */}
+            <LogOutBtn />
         </div>
       </nav>
   );
