f379fcfd508b9775af10c819e08ffb1dd7f78c02
j-o-phillips <jphillips105@googlemail.com>
2023-10-31 17:40:26 +0100

    navbar add member

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,30 +1,29 @@
 "use client";
 
-import { useContext, useEffect } from "react";
+// import { useContext } from "react";
 import LogOutBtn from "../LogOutBtn/LogOutBtn";
 import style from "./Navbar.module.css";
-// import Image from "next/image";
+// import { UserContext } from "@/context/UserContext";
 import { useSession } from "next-auth/react";
-import { UserContext } from "@/context/UserContext";
 
 function Navbar() {
+  // const { user } = useContext(UserContext);
   const session = useSession();
-  const { user } = useContext(UserContext);
-
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
 
   return (
     <nav className={style.nav}>
       <div className={style.logo}>Image</div>
-      <div className={style.end}>
-        <div>{user}</div>
-        <button className={style.button}>
-          LogOut
+      {session.status === "authenticated" && (
+        <div className={style.end}>
+          <h4>{session.data.user.name}</h4>
+          <img
+            src={session.data.user.image}
+            alt="image"
+            className={style.image}
+          />
           <LogOutBtn />
-        </button>
-      </div>
+        </div>
+      )}
     </nav>
   );
 }
--- components/SideBar/SideBar.jsx
+++ components/SideBar/SideBar.jsx
@@ -1,20 +1,32 @@
-"use client"
-import style from './SideBar.module.css'
+"use client";
+import style from "./SideBar.module.css";
+import { useContext } from "react";
+import { UserContext } from "@/context/UserContext";
+import Link from "next/link";
 function SideBar() {
+  const { user } = useContext(UserContext);
+
   return (
     <div className={style.container}>
       <div className={style.sidebar}>
-        <div className={style.icons}>
-          <NavItem icon="🏠" label="Home" />
-          <NavItem icon="📊" label="Analytics" />
-          <NavItem icon="⚙️" label="Settings" />
-        </div>
-        <div className={style.copyright}>
-          &copy; 2023 Chores App
-        </div>
+        {user && (
+          <div className={style.icons}>
+            <NavItem icon="🏠" label="Home" />
+            <NavItem icon="📊" label="Analytics" />
+            <NavItem icon="⚙️" label="Settings" />
+            <Link href={`/${user.households[0]}/addMember`}>Add Member</Link>
+            <button
+              onClick={() => {
+                console.log(user);
+              }}
+            >
+              Print
+            </button>
+          </div>
+        )}
+        <div className={style.copyright}>&copy; 2023 Chores App</div>
       </div>
-      <div className={style.content}>
-      </div>
+      <div className={style.content}></div>
     </div>
   );
 }
